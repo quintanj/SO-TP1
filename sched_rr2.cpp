e27d619d5d233da9b3f2575bd938db9a06c9ff16
aristidescata <arsitidescata@gmail.com>
2015-08-31 22:56:28 -0300

    Agregue el ej4, la carpeta con un lote y el tex para el informe

--- sched_rr2.cpp
+++ sched_rr2.cpp
@@ -8,6 +8,18 @@ using namespace std;
 
 SchedRR2::SchedRR2(vector<int> argn) {
 	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
+	
+	quantums.resize(argn[0]);
+	colaReady.resize(argn[0]);				//cantidad de colas = cantidad de cpus
+	quantumsActuales.resize(argn[0]);
+	cantTasks.resize(argn[0]);				//cantidad de tareas de cada cpu
+	for(int i = 0; i < argn[0]; i++){
+		quantums[i] = argn[i + 1];
+				
+		}
+	quantumsActuales = quantums;
+	
+	
 }
 
 SchedRR2::~SchedRR2() {
@@ -16,6 +28,10 @@ SchedRR2::~SchedRR2() {
 
 
 void SchedRR2::load(int pid) {
+	int cpu = cpuMenosTareas();		//busco cpu con menos tareas
+	cantTasks[cpu]++;				//sumo una tarea al cpu
+	colaReady[cpu].push(pid);		//agrego el pid a la colaReady del cpu con menos tareas
+	
 }
 
 void SchedRR2::unblock(int pid) {
--- sched_rr2.h
+++ sched_rr2.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <queue>
+#include <tuple>
 #include "basesched.h"
 
 class SchedRR2 : public SchedBase {
@@ -12,6 +13,14 @@ class SchedRR2 : public SchedBase {
 		virtual void load(int pid);
 		virtual void unblock(int pid);
 		virtual int tick(int cpu, const enum Motivo m);
+		
+		
+		std::vector<queue<int> > colaReady; 				//vector de colasReady de cada cpu
+		std::vector<int> quantums; 							//vector de quantums de cada cpu
+		std::vector<int> quantumsActuales; 					//vector de quantumsActuales de cada cpu
+		std::vector<int> cantTasks;							//cantidad de tareas en un cpu BLOCKED + READY + RUNNING
+		std::queue<tuple<int, int> > cpuTareaBloqueada;	 	//vector de quantumsActuales de cada cpu
+		
 };
 
 #endif
